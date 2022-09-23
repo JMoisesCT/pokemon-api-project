77439c7436a1c6e871e9a17cf679fa3ad4ca4474
Moises Cumpa Torres <moises.cumpa@gmail.com>
2022-09-23 04:16:27 -0500

    Added detection of many types of pokemon

--- Assets/Scripts/UI/PokemonSelected/CanvasPokemonDetail.cs
+++ Assets/Scripts/UI/PokemonSelected/CanvasPokemonDetail.cs
@@ -6,10 +6,17 @@ using UnityEngine.UI;
 
 public class CanvasPokemonDetail : MonoBehaviour
 {
+    [Serializable]
+    public class PokemonTypeContainer
+    {
+        public Image imageContainer;
+        public Text textType;
+    }
+
     [SerializeField] private RawImage _imagePokemon;
     [SerializeField] private Text _textPokemonNumber;
     [SerializeField] private Text _textPokemonName;
-    [SerializeField] private Text _textPokemonType;
+    [SerializeField] private PokemonTypeContainer[] _pokemonTypeList;
     [SerializeField] private Text _textPokemonHeight;
     [SerializeField] private Text _textPokemonWeight;
     [SerializeField] private Button _buttonClose;
@@ -51,8 +58,21 @@ public class CanvasPokemonDetail : MonoBehaviour
         _imagePokemon.texture = pokemonDetail.imageDefault;
         _textPokemonNumber.text = pokemonDetail.id.GetPokemonFormat();
         _textPokemonName.text = pokemonDetail.name;
-        _textPokemonType.text = pokemonDetail.types[0];
+        LoadTypes(pokemonDetail);
         _textPokemonHeight.text = pokemonDetail.height;
         _textPokemonWeight.text = pokemonDetail.weight;
+    }
+
+    private void LoadTypes(DataManager.PokemonDetail pokemonDetail)
+    {
+        for (int i = 0; i < _pokemonTypeList.Length; ++i)
+        {
+            _pokemonTypeList[i].imageContainer.gameObject.SetActive(false);
+        }
+        for (int i = 0; i < pokemonDetail.types.Length; ++i)
+        {
+            _pokemonTypeList[i].imageContainer.gameObject.SetActive(true);
+            _pokemonTypeList[i].textType.text = pokemonDetail.types[i];
+        }
     }
 }
